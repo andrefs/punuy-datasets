47eca95fde8ee388f6ab9e8ea983cd86941b7014
André Santos <andrefs@andrefs.com>
2023-11-21 22:57:34 +0000

    wip

--- index.ts
+++ index.ts
@@ -1,11 +1,22 @@
 
-export * from './bg1000';
-export * from './mc30';
-export * from './mm907';
-export * from './ragm287';
-export * from './rg65';
-export * from './srw2034';
-export * from './ws353';
-export * from './ws353gs';
-export * from './yp130';
+import bg1000 from './bg1000';
+import mc30 from './mc30';
+import mm907 from './mm907';
+import ragm287 from './ragm287';
+import rg65 from './rg65';
+import srw2034 from './srw2034';
+import ws353 from './ws353';
+import ws353gs from './ws353gs';
+import yp130 from './yp130';
 
+export {
+  bg1000,
+  mc30,
+  mm907,
+  ragm287,
+  rg65,
+  srw2034,
+  ws353,
+  ws353gs,
+  yp130,
+};
