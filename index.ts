9efeebd32344f51550cfb7eebef1257d967b8991
André Santos <andrefs@andrefs.com>
2023-11-21 22:39:07 +0000

    wip

---
+++ index.ts
@@ -0,0 +1,22 @@
+
+import bg1000 from './datasets/bg1000.json';
+import mc30 from './datasets/mc30.json';
+import mm907 from './datasets/mm907.json';
+import ragm287 from './datasets/ragm287.json';
+import rg65 from './datasets/rg65.json';
+import srw2034 from './datasets/srw2034.json';
+import ws353 from './datasets/ws353.json';
+import ws353gs from './datasets/ws353gs.json';
+import yp130 from './datasets/yp130.json';
+
+export {
+  bg1000,
+  mc30,
+  mm907,
+  ragm287,
+  rg65,
+  srw2034,
+  ws353,
+  ws353gs,
+  yp130,
+};
