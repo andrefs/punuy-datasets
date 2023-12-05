28ee90c2d1e6ab8313f9bf026b0e0138ab22dd21
André Santos <andrefs@andrefs.com>
2023-12-05 17:50:12 +0000

    prettier and eslint

--- bg1000.ts
+++ bg1000.ts
@@ -1,4 +1,3 @@
-
-import bg1000 from './datasets/bg1000.json';
-import { DatasetProfile } from './src/lib/types';
+import bg1000 from "./datasets/bg1000.json";
+import { DatasetProfile } from "./src/lib/types";
 export default bg1000 as DatasetProfile;
