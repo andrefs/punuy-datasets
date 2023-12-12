b6d22efc832d3ff2fe53112f97806b8a62960337
André Santos <andrefs@andrefs.com>
2023-12-12 23:16:34 +0000

    mesh2

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,18 @@ import mc30 from "./datasets/mc30";
 import mayoSRS from "./datasets/mayoSRS";
 import minimayoSRS from "./datasets/minimayoSRS";
 import umnsrsSimMod from "./datasets/umnsrsSimMod";
+
 import umnsrsRelMod from "./datasets/umnsrsRelMod";
 import mturk771 from "./datasets/mturk771";
 import rg65 from "./datasets/rg65";
 import srw2034 from "./datasets/srw2034";
 import ws353 from "./datasets/ws353";
+
 import ws353Rel from "./datasets/ws353Rel";
 import ws353Sim from "./datasets/ws353Sim";
 import yp130 from "./datasets/yp130";
 import ps65 from "./datasets/ps65";
+import mesh2 from "./datasets/mesh2";
 
 export {
   bg1000,
@@ -19,13 +22,16 @@ export {
   mayoSRS,
   minimayoSRS,
   umnsrsSimMod,
+  //
   umnsrsRelMod,
   mturk771,
   rg65,
   srw2034,
   ws353,
+  //
   ws353Sim,
   ws353Rel,
   yp130,
   ps65,
+  mesh2,
 };
