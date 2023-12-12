3d5fce9054bc3edb5b486d193f4cd4d4995ea38b
André Santos <andrefs@andrefs.com>
2023-12-12 23:45:32 +0000

    MeSH1 is the same as MiniMayoSRS

--- src/datasets/mesh1.ts
+++
@@ -1,3 +0,0 @@
-import profile from "../../profiles/mesh1.json";
-import { DatasetProfile } from "../lib/types";
-export default profile as DatasetProfile;
--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,6 @@ import yp130 from "./datasets/yp130";
 import ps65 from "./datasets/ps65";
 import mesh2 from "./datasets/mesh2";
 
-import mesh1 from "./datasets/mesh2";
-
 export {
   bg1000,
   mc30,
@@ -36,6 +34,4 @@ export {
   yp130,
   ps65,
   mesh2,
-  //
-  mesh1,
 };
