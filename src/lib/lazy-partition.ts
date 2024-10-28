8a62fff68248991274251dab4afa282d17f18cb1
André Santos <andrefs@andrefs.com>
2024-10-28 14:35:52 +0000

    script to check invalid chars

--- src/lib/lazy-partition.ts
+++ src/lib/lazy-partition.ts
@@ -2,10 +2,53 @@ import { Partition, PartitionData } from "./types";
 import fs from "fs";
 import logger from "./logger";
 
+export function fixPartData(
+  part: PartitionData,
+  options: LazyPartitionOptions = {
+    trim: true,
+    replaceUTF8Quotes: true,
+    noUnderScores: true,
+  }
+): PartitionData {
+  let term1 = part.term1;
+  let term2 = part.term2;
+
+  if (options.replaceUTF8Quotes) {
+    term1 = term1
+      .replace(/’/g, "'")
+      .replace(/‘/g, "'")
+      .replace(/”/g, '"')
+      .replace(/“/g, '"');
+    term2 = term2
+      .replace(/’/g, "'")
+      .replace(/‘/g, "'")
+      .replace(/”/g, '"')
+      .replace(/“/g, '"');
+  }
+  if (options.noUnderScores) {
+    term1 = term1.replace(/_+/g, " ");
+    term2 = term2.replace(/_+/g, " ");
+  }
+  if (options.trim) {
+    term1 = term1.trim();
+    term2 = term2.trim();
+  }
+  return {
+    ...part,
+    term1,
+    term2,
+  };
+}
+
+interface LazyPartitionOptions {
+  trim?: boolean;
+  replaceUTF8Quotes?: boolean;
+  noUnderScores?: boolean;
+}
 export function lazyPartition(
   part: Omit<Partition, "data">,
   path: string,
-  { trim = true, replaceUTF8Quotes = true } = {}
+  options: LazyPartitionOptions = {}
 ): Partition {
   let _data: PartitionData[] | null = null;
 
@@ -17,31 +60,7 @@ export function lazyPartition(
           _data = JSON.parse(fs.readFileSync(path, "utf-8")) as PartitionData[];
         }
 
-        return _data.map(d => {
-          let term1 = d.term1;
-          let term2 = d.term2;
-          if (trim) {
-            term1 = term1.trim();
-            term2 = term2.trim();
-          }
-          if (replaceUTF8Quotes) {
-            term1 = term1
-              .replace(/’/g, "'")
-              .replace(/‘/g, "'")
-              .replace(/”/g, '"')
-              .replace(/“/g, '"');
-            term2 = term2
-              .replace(/’/g, "'")
-              .replace(/‘/g, "'")
-              .replace(/”/g, '"')
-              .replace(/“/g, '"');
-          }
-          return {
-            ...d,
-            term1,
-            term2,
-          };
-        });
+        return _data.map(d => fixPartData(d, options));
       }
       return Reflect.get(target, prop, receiver);
     },
---
+++ src/scripts/check-chars.ts
@@ -0,0 +1,80 @@
+import fs from "fs/promises";
+import { PartitionData } from "src/lib/types";
+import path from "path";
+import { fixPartData } from "../lib/lazy-partition";
+
+const SRC_FOLDER = process.argv[2];
+if (!SRC_FOLDER) {
+  console.error("Usage: ts-node check-chars.ts <src-folder>");
+  process.exit(1);
+}
+
+async function readFoldersInFolder(folder: string) {
+  const files = await fs.readdir(folder, { withFileTypes: true });
+  return files.filter(f => f.isDirectory()).map(f => f.name);
+}
+
+async function readPartitionsInFolder(folder: string) {
+  const files = await fs.readdir(folder, { withFileTypes: true });
+  return files
+    .filter(f => f.name.endsWith(".json") && !f.name.endsWith("dataset.json"))
+    .map(f => f.name);
+}
+
+async function readPartitionFile(file: string) {
+  const data = await fs.readFile(file, "utf-8");
+  const part = JSON.parse(data) as PartitionData[];
+  return part;
+}
+
+function checkPartition(partName: string, part: PartitionData[]): boolean {
+  // Regular expression to match valid word characters including accents, apostrophes, and digits.
+  const validCharRegex = /^[-.?+&,()/\p{L}\p{M}\p{N}0-9' ]+$/u;
+
+  let allValid = true;
+
+  for (const [i, d] of part.entries()) {
+    const fixed = fixPartData(d);
+    for (const t of ["term1", "term2"]) {
+      const str = fixed[t as keyof PartitionData] as string;
+      if (!validCharRegex.test(str)) {
+        allValid = false;
+        const invalidChars = str
+          .split("")
+          .map((char, pos) =>
+            validCharRegex.test(char) ? null : { char, pos }
+          )
+          .filter(Boolean);
+
+        console.warn(
+          `Warning: Invalid characters in "${t}" of pair ("${fixed.term1}", "${fixed.term2}") at ${partName}, index ${i}:`
+        );
+        for (const { char, pos } of invalidChars.filter(ic => !!ic)) {
+          console.warn(`  - Invalid character "${char}" at position ${pos}`);
+        }
+      }
+    }
+  }
+  if (allValid) {
+    console.log(`All characters in ${partName} are valid`);
+  }
+
+  return allValid;
+}
+async function main() {
+  const folders = await readFoldersInFolder(SRC_FOLDER);
+  for (const folder of folders) {
+    const partitions = await readPartitionsInFolder(`${SRC_FOLDER}/${folder}`);
+    for (const partFile of partitions) {
+      const part = await readPartitionFile(
+        path.join(SRC_FOLDER, folder, partFile)
+      );
+      checkPartition(`${folder}/${partFile}`, part);
+    }
+    console.log(partitions);
+  }
+}
+
+main()
+  .then(() => console.log("Done"))
+  .catch(console.error);
