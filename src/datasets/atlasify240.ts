eed61249d1b56678147535563a540ed780c7c96f
André Santos <andrefs@andrefs.com>
2024-11-19 18:42:33 +0000

    fix: Update import paths for dataset and folder in atlasify240.ts

--- src/datasets/atlasify240.ts
+++ src/datasets/atlasify240.ts
@@ -1,8 +1,8 @@
 import { DatasetProfile, Partition } from "../lib/types";
 import { lazyPartition } from "../lib/lazy-partition";
-import dataset from "../../profiles/atlasify240/dataset.json";
+import dataset from "profiles/atlasify240/dataset.json";
 import path from "path";
-const folder = "../../profiles/atlasify240";
+const folder = "profiles/atlasify240";
 
 const ds = {
   ...dataset,
