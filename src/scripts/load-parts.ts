25714504acf1e22dbc70b78c8c4493f11fc97469
André Santos <andrefs@andrefs.com>
2025-06-09 13:16:14 +0100

    script to test loading partitions

--- src/scripts/load-parts.ts
+++ src/scripts/load-parts.ts
@@ -1,4 +1,8 @@
+import * as datasets from '..';
 
 
-
+for (const ds of Object.values(datasets)) {
+  for (const part of ds.partitions)
+    console.log(`${ds.id}#${part.id}: ${part.data.length}`);
+}
 
