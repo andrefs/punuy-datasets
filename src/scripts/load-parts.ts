7b3bd58cfb4f68b330aa5b2ed8d67cc7811517ce
André Santos <andrefs@andrefs.com>
2025-06-10 23:53:52 +0100

    12.0.0

--- src/scripts/load-parts.ts
+++ src/scripts/load-parts.ts
@@ -1,8 +1,6 @@
-import * as datasets from '..';
-
+import * as datasets from "..";
 
 for (const ds of Object.values(datasets)) {
   for (const part of ds.partitions)
     console.log(`${ds.id}#${part.id}: ${part.data.length}`);
 }
-
